1a390fc5fa8e1bbfabc11e5e7e37cae57e76f0b0
solomakhin-xrs <ssa@ari-on.ru>
2026-07-23 16:56:56 +0300

    DIAG B segment during optimizing B1 task

--- src/Algorithms/Diagnostics/StableSegment.cs
+++ src/Algorithms/Diagnostics/StableSegment.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using System.Reflection.Emit;
+
 namespace Algorithms.Diagnostics;
 
 public static class StableSegment
@@ -6,6 +9,52 @@ public static class StableSegment
         IReadOnlyList<int> values,
         int maxDifference)
     {
-        return (0, 0);
+        if (!values.Any()) return (0, 0);
+
+        if (values == null)
+        {
+            string? paramName = "List of values can't be null.";
+            throw new ArgumentNullException(paramName);
+        }
+
+        if (maxDifference < 0)
+        {
+            string? paramName = "Max difference value should be not negative.";
+            throw new ArgumentOutOfRangeException(paramName);
+        }
+
+        if (values.Count == 1) return (0, 1);
+
+        var dict = new Dictionary<int, int>(values.Count);
+
+        for (int i = values.Count; i > 1; --i)
+        {
+            int iterCount = 0;
+
+            for (int j = 0; j < values.Count - i + 1; j++)
+            {
+                var tmp = values.Skip(j).Take(i);
+                var max = tmp.Max();
+                var min = tmp.Min();
+
+                if (Math.Abs((long)max - min) <= maxDifference)
+                {
+                    dict.Add(j, i);
+                    iterCount++;
+                }
+            }
+
+            if (iterCount > 0) break;
+        }
+
+        if (dict.Count > 1)
+        {
+            var res = dict.MinBy(x => x.Key);
+            return (res.Key, res.Value);
+        }
+
+        if (dict.Count == 0) return (0, 1);
+
+        return (dict.First().Key, dict.First().Value);
     }
 }
--- tests/Algorithms.Tests/Diagnostics/StableSegmentTests.cs
+++ tests/Algorithms.Tests/Diagnostics/StableSegmentTests.cs
@@ -13,6 +13,106 @@ public class StableSegmentTests
 
         var maxmin = StableSegment.LongestStableSegment(list, diff);
 
-        Assert.Equal(maxmin, (0, 5));
+        Assert.Equal(maxmin, (0, 2));
+    }
+
+    [Fact]
+    public void OverflowCheck()
+    {
+        var list = new List<int> { int.MaxValue, int.MinValue };
+        var diff = 1;
+
+        var maxmin = StableSegment.LongestStableSegment(list, diff);
+
+        Assert.Equal(maxmin, (0, 1));
+    }
+
+    [Fact]
+    public void NoSegments()
+    {
+        var list = new List<int> { 8, 2, 10 };
+        var diff = 1;
+
+        var maxmin = StableSegment.LongestStableSegment(list, diff);
+
+        Assert.Equal(maxmin, (0, 1));
+    }
+
+    [Fact]
+    public void SegmentInTheEnd()
+    {
+        var list = new List<int> { 16, 200, 83, 7, 14, 21 };
+        var diff = 7;
+
+        var maxmin = StableSegment.LongestStableSegment(list, diff);
+
+        Assert.Equal(maxmin, (3, 2));
+    }
+
+    [Fact]
+    public void ListIsEmty()
+    {
+        var list = new List<int> { };
+        var diff = 100;
+
+        var maxmin = StableSegment.LongestStableSegment(list, diff);
+
+        Assert.Equal(maxmin, (0, 0));
+    }
+
+    [Fact]
+    public void HasSegmentWithNegativeValues()
+    {
+        var list = new List<int> { 7, 2, -3, 2, 17 };
+        var diff = 5;
+
+        var maxmin = StableSegment.LongestStableSegment(list, diff);
+
+        Assert.Equal(maxmin, (1, 3));
+    }
+
+    [Fact]
+    public void MoreThanOneSegment()
+    {
+        var list = new List<int> { 1, 100, 500, 1200, 0, 79, 90, 15, 1040, 203 };
+        var diff = 99;
+
+        var maxmin = StableSegment.LongestStableSegment(list, diff);
+
+        Assert.Equal(maxmin, (4, 4));
+    }
+
+    [Fact]
+    public void TwoEqualSegmentsInARow()
+    {
+        var list = new List<int> { 5, 14, 23, 0, 9, 18 };
+        var diff = 9;
+
+        var maxmin = StableSegment.LongestStableSegment(list, diff);
+
+        Assert.Equal(maxmin, (0, 2));
+    }
+
+    [Fact]
+    public void TwoEqualSegmentsAfterSpaceElement()
+    {
+        var list = new List<int> { 0, 9, 18, 130, 5, 14, 23 };
+        var diff = 18;
+
+        var maxmin = StableSegment.LongestStableSegment(list, diff);
+
+        Assert.Equal(maxmin, (0, 3));
+    }
+
+    [Fact]
+    public void ThrowsArgumentOutOfRangeException()
+    {
+        List<int> list = new List<int> { 0, 9, 18, 13, 5, 14, 23 };
+        var diff = -1;
+
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            StableSegment.LongestStableSegment(list, diff);
+        });
     }
 }
